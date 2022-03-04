af1cd90af4262daa1073200f79242d4da5a98c6d
EK <EK@EK>
2022-03-04 11:03:18 +0100

    differente solution

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 const hostname = '127.0.0.1';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  switch (req.url) {
-    case '/about':
-      fs.readFile('about.html', (err, data) => {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-      });
-    case '/contact-me':
-      fs.readFile('contact-me.html', (err, data) => {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-      });
-    case '/':
-      fs.readFile('index.html', (err, data) => {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-      });
-    default:
+  const q = url.parse(req.url, true);
+  let filename = q.pathname === '/' ? 'index.html' : '.' + q.pathname + '.html';
+  fs.readFile(filename, (err, data) => {
+    if (err) {
+      console.log(err);
       fs.readFile('404.html', (err, data) => {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.writeHead(404, { 'Content-Type': 'text/html' });
         res.write(data);
-        return res.end();
+        res.end();
       });
-  }
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+    }
+  });
 });
 
 server.listen(port, () => {
